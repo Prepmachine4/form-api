95d691a9fb44efe5bb282fee7575b861e043a8f4
ssq <sunshiqi447447@163.com>
2022-05-05 15:59:33 +0800

    form加入is_template属性

--- app/models.py
+++ app/models.py
@@ -10,19 +10,17 @@ class User(db.Document):
     password = db.StringField(required=True)  # 用户密码
     role = db.StringField(required=True)  # 用户角色：个人或企业
 
-
 # 对应数据库中的form集合
 class form(db.Document):
-    _id = db.ObjectIdField(primary_key=True)  # 表单id，mongdb自动生成
-    user_id = db.ObjectIdField(required=True)  # 设置表单的用户id
-    category = db.StringField(required=True)  # 表单类型：问卷型或业务型
-    name = db.StringField(required=True)  # 表单名
-    struct = db.StringField(required=True)  # 表单结构
-    status = db.IntField(required=True)  # 表单状态
-    create_time = db.StringField(required=True)  # 表单的发布时间
-    end_time = db.StringField(required=True)  # 表单发布后截止时间
-    url = db.StringField()  # 表单发布后对应的url
-
+    _id = db.ObjectIdField(primary_key=True)    # 表单id，mongdb自动生成
+    user_id = db.ObjectIdField(required=True)    # 设置表单的用户id
+    category = db.StringField(required=True)   # 表单类型：问卷型或业务型
+    name = db.StringField(required=True)    # 表单名
+    struct = db.StringField(required=True) # 表单结构
+    create_time = db.StringField(required=True)    #表单的发布时间
+    end_time = db.StringField(default="")   # 表单发布后截止时间
+    is_template = db.BooleanField(required=True)    #表单是否保存为模板
+    url = db.StringField() # 表单发布后对应的url
 
 # 对应数据库中的form_data集合
 class form_data(db.Document):
