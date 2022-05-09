299b4ef7a151a2ab4022227002f8d96b43d3b344
ssq <sunshiqi447447@163.com>
2022-05-09 17:20:40 +0800

    修改“修改用户信息”功能

--- app/login.py
+++ app/login.py
@@ -8,13 +8,12 @@ bp_user = Blueprint("user", __name__)  # 创建蓝图，该蓝图管理用户相
 
 @bp_user.route('/login', methods=['POST'])
 def login():
-
     # 通过邮箱和密码登录
-    form_info = request.get_data()
-    form_info = json.loads(form_info.decode("UTF-8"))
+    user_info = request.get_data()
+    user_info = json.loads(user_info.decode("UTF-8"))
 
-    user = User.objects(email=form_info.get("email"),
-                        password=form_info.get("password")).first()
+    user = User.objects(email=user_info.get("email"),
+                        password=user_info.get("password")).first()
     if user:
         return jsonify({"user_info": {"_id": str(user._id),
                                       "email": user.email,
@@ -26,34 +25,38 @@ def login():
 
 @bp_user.route('/register', methods=['POST'])
 def register():
-
     # 注册需要邮箱、密码、用户类型
-    form_info = request.get_data()
-    form_info = json.loads(form_info.decode("UTF-8"))
+    user_info = request.get_data()
+    user_info = json.loads(user_info.decode("UTF-8"))
 
-    user = User.objects(email=form_info.get("email")).first()
+    user = User.objects(email=user_info.get("email")).first()
     if user:
         return jsonify({"message":"The user already exists!"}), 400
     else:
         user = User(_id=ObjectId(),
-                    email=form_info.get("email"),
-                    password=form_info.get("password"),
-                    role=form_info.get("type"))
+                    email=user_info.get("email"),
+                    password=user_info.get("password"),
+                    role=user_info.get("type"),
+                    nickname="")
         user.save()
         return jsonify({})
 
 
 @bp_user.route('/profile', methods=['PUT'])
 def profile():
-
     # 修改密码，传入email、nickname、password
-    form_info = request.get_data()
-    form_info = json.loads(form_info.decode("UTF-8"))
+    user_info = request.get_data()
+    user_info = json.loads(user_info.decode("UTF-8"))
 
-    user = User.objects(email=form_info.get("email")).first()
+    email = user_info.get("email")
+    password = user_info.get("password")
+    nickname = user_info.get("nick_name")
+
+    user = User.objects(email=email).first()
     if user:
-        user.update(nickname=form_info.get("nick_name"),
-                    password=form_info.get("password"))
+        user.update(password=password)
+        if nickname != None:
+            user.update(nickname=nickname)
         return jsonify({})
     else:
         return jsonify({"message": "No such user!"}), 400
