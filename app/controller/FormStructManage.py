27b0a2f7f9d79db6a25b2191c1fd51dc0ab56db7
ssq <sunshiqi447447@163.com>
2022-05-15 13:12:22 +0800

    修改代码结构（apifox的结构）

--- app/controller/FormStructManage.py
+++ app/controller/FormStructManage.py
@@ -1,5 +1,6 @@
-from flask import json
-from app.model import Form
+from flask import request, json
+from bson import ObjectId
+from app.model import Form, FormData
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from . import bp_form
 
@@ -56,6 +57,43 @@ def getUserForms(user_id):
                            "create_time": create_time, "setting": {"end_time": end_time}}]
 
     return json.jsonify(list_data)
+
+
+@bp_form.route('/<form_id>', methods=['DELETE'])
+@jwt_required(optional=False)
+def deleteForm(form_id):
+    '''删除某个表单结构'''
+    Form.objects(_id=form_id).delete()
+
+    # 删除填写的数据
+    FormData.objects(form_id=form_id).delete()
+    return json.jsonify({})
+
+
+@bp_form.route('/<user_id>', methods=['POST'])
+@jwt_required(optional=False)
+def saveForm(user_id):
+    '''保存用户表单结构'''
+    form_info = request.get_data()
+    form_info = json.loads(form_info.decode("UTF-8"))
+
+    is_template = form_info.get("is_template")
+    name = form_info.get("name")
+    struct = form_info.get("struct")
+    category = form_info.get("category")
+    create_time = form_info.get("create_time")
+
+    user_form = Form(_id=ObjectId(),
+                     user_id=user_id,
+                     category=category,
+                     name=name,
+                     struct=struct,
+                     create_time=create_time,
+                     end_time="",
+                     is_template=is_template)
+    user_form.save()
+
+    return json.jsonify({"_id": str(user_form._id)})
 
 
 @bp_form.route('/struct/<form_id>', methods=['GET'])
