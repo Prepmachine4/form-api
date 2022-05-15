8710fcc76915abeeb972aa31b8a5f4b9f3146b57
yimi <1203218616@qq.com>
2022-05-15 20:36:48 +0800

    model内修改user，增加post

--- app/model/User.py
+++ app/model/User.py
@@ -7,3 +7,8 @@ class User(db.Document):
     password = db.StringField(required=True)  # 用户密码
     role = db.StringField(required=True)  # 用户类型：个人或企业
     nickname = db.StringField(default="")  # 用户昵称
+    phone = db.StringField(default="")  #用户手机号
+    gender = db.StringField(default="")  #用户性别：男 女 其他
+    dept_id = db.ObjectIdField(default="")  #用户部门ID
+    post_id = db.ObjectIdield(default="")  #用户岗位ID
+    enterprise_id = db.ObjectIdField(default="")  #用户企业ID
