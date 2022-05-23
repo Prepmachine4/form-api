0ccdb6274b958ae86b08441b332afee47dfd8af2
ssq <sunshiqi447447@163.com>
2022-05-23 23:47:48 +0800

    审批Audit的index数据类型改为int

--- app/model/Audit.py
+++ app/model/Audit.py
@@ -4,7 +4,7 @@ class Audit(db.Document):
     _id = db.ObjectIdField(primary_key=True)    #审批信息的id
     user_id = db.ObjectIdField()  #审批人id
     formdata_id = db.ObjectIdField()    #审批的表单数据id
-    index = db.StringField()    #审批顺序
+    index = db.IntField()    #审批顺序
     ip = db.StringField()   #审批ip
     opinion = db.StringField()  #审批意见
     sign = db.StringField() #审批签名
